6fa0c6130476e86b2c47a938f11c85360012d9eb
stormk539 <stormk539@users.noreply.github.com>
2019-12-05 14:41:13 -0500

    adsf
    asdf

--- .cache/behavior-hx/SceneEvents_1.hx
+++ .cache/behavior-hx/SceneEvents_1.hx
@@ -62,19 +62,16 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class SceneEvents_0 extends SceneScript
+class SceneEvents_1 extends SceneScript
 {
-	public var _IsPunching:Bool;
-	public var _score:Float;
+	public var _MusicPlaying:Bool;
 	
 	
 	public function new(dummy:Int, dummy2:Engine)
 	{
 		super();
-		nameMap.set("IsPunching", "_IsPunching");
-		_IsPunching = false;
-		nameMap.set("score", "_score");
-		_score = 0;
+		nameMap.set("MusicPlaying", "_MusicPlaying");
+		_MusicPlaying = true;
 		
 	}
 	
@@ -82,56 +79,75 @@ class SceneEvents_0 extends SceneScript
 	{
 		
 		/* ======================== When Creating ========================= */
-		_score = 0;
 		Engine.engine.setGameAttribute("Player_Health", 3);
-		playSound(getSound(200));
-		
-		/* ======================== Actor of Type ========================= */
-		addWhenTypeGroupKilledListener(getActorType(167), function(eventActor:Actor, list:Array<Dynamic>):Void
+		getActor(1).moveTo(98, 84, 1, Easing.expoInOut);
+		runLater(1000 * 1.05, function(timeTask:TimedTask):Void
 		{
-			if(wrapper.enabled)
-			{
-				_score = (_score + 100);
-			}
-		});
+			getActor(1).moveTo(352, 64, 0.75, Easing.expoInOut);
+		}, null);
+		runLater(1000 * 2, function(timeTask:TimedTask):Void
+		{
+			getActor(2).moveTo(98, 84, 1, Easing.expoInOut);
+		}, null);
+		runLater(1000 * 3.05, function(timeTask:TimedTask):Void
+		{
+			getActor(2).moveTo(352, 225, 0.5, Easing.expoInOut);
+		}, null);
+		runLater(1000 * 0.5, function(timeTask:TimedTask):Void
+		{
+			getActor(4).setAnimation("Second");
+		}, null);
+		runLater(1000 * 2.5, function(timeTask:TimedTask):Void
+		{
+			getActor(4).setAnimation("Third");
+		}, null);
+		
+		/* ======================== When Creating ========================= */
+		if((_MusicPlaying == true))
+		{
+			setVolumeForChannel(50/100, 0);
+			loopSoundOnChannel(getSound(198), 0);
+			_MusicPlaying = false;
+		}
+		
+		/* ======================== When Creating ========================= */
+		getActor(5).moveTo(160, -46, 1, Easing.elasticIn);
+		
+		/* ======================== When Creating ========================= */
+		getActor(3).alpha = 0 / 100;
+		runLater(1000 * 0.5, function(timeTask:TimedTask):Void
+		{
+			getActor(3).alpha = 100 / 100;
+		}, null);
+		runLater(1000 * 1, function(timeTask:TimedTask):Void
+		{
+			getActor(3).alpha = 0 / 100;
+		}, null);
+		runLater(1000 * 2.5, function(timeTask:TimedTask):Void
+		{
+			getActor(3).alpha = 100 / 100;
+		}, null);
+		runLater(1000 * 3, function(timeTask:TimedTask):Void
+		{
+			getActor(3).alpha = 0 / 100;
+		}, null);
 		
 		/* ========================= When Drawing ========================= */
 		addWhenDrawingListener(null, function(g:G, x:Float, y:Float, list:Array<Dynamic>):Void
 		{
 			if(wrapper.enabled)
 			{
-				g.setFont(getFont(184));
-				g.drawString("" + "Score:", 15, 290);
-				g.drawString("" + _score, 95, 290);
+				g.setFont(getFont(204));
+				g.drawString("" + "Clench arm to begin", 260, 167);
 			}
 		});
 		
-		/* ======================== When Updating ========================= */
-		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		/* =========================== Keyboard =========================== */
+		addKeyStateListener("down", function(pressed:Bool, released:Bool, list:Array<Dynamic>):Void
 		{
-			if(wrapper.enabled)
+			if(wrapper.enabled && pressed)
 			{
-				if((_score == 600))
-				{
-					switchScene(GameModel.get().scenes.get(2).getID(), null, createCrossfadeTransition(0.25));
-				}
-				if(((Engine.engine.getGameAttribute("Player_Health") : Float) == 2))
-				{
-					getActor(1).setAnimation("Hurt");
-				}
-				else if(((Engine.engine.getGameAttribute("Player_Health") : Float) == 1))
-				{
-					getActor(1).setAnimation("Hurt2");
-				}
-			}
-		});
-		
-		/* ======================== Actor of Type ========================= */
-		addWhenTypeGroupKilledListener(getActorType(169), function(eventActor:Actor, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled)
-			{
-				_score = (_score + 75);
+				switchScene(GameModel.get().scenes.get(1).getID(), null, createCrossfadeTransition(0.25));
 			}
 		});
 		
